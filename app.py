fa16603e93957596e14993c960aa9ffff1e8bd44
Jose Manuel Marron <manemarron@gmail.com>
2015-11-15 12:30:11 -0600

    cambios

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import argparse
-from TwitterUtils import TwitterUtils, fix_tweets
+
 from DbUtils import DbUtils
+from TwitterUtils import TwitterUtils, fix_tweets
 
 parser = argparse.ArgumentParser(description="Tweet options")
 parser.add_argument('q', help="query")
 parser.add_argument('-n', dest="n", type=int, default=1, help="Number of requests")
+parser.add_argument('-result-type', dest="result_type", default="mixed", help="Result type")
 parser.add_argument('-max-id', dest="max_id", default=None, help="Maximum Id from which to start searching")
 parser.add_argument('-until', dest="until", default=None, help="Last date")
 parser.add_argument('-lang', dest="lang", default=None, help="Language")
 args = parser.parse_args()
 q = args.q
 n = args.n
+result_type = args.result_type
 max_id = args.max_id
 until = args.until
 lang = args.lang
@@ -18,7 +21,9 @@ lang = args.lang
 twitter = TwitterUtils()
 db = DbUtils()
 
-next_results = {"q": q, "result_type": "mixed", "count": 100}
+next_results = {"q": q, "count": 100}
+if result_type is not None:
+    next_results["result_type"] = result_type
 if max_id is not None:
     next_results["max_id"] = max_id
 if until is not None:
