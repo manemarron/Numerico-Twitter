f9fdfb93e8f5d7c0df5bfe84f609f852891f3649
Jose Manuel Marron <manemarron@gmail.com>
2015-12-01 16:15:14 -0600

    codigo y 3d en r

--- princomp_tweets.R
+++ princomp_tweets.R
@@ -1,15 +1,27 @@
 library(Matrix)
 library(ggplot2)
 library(ggfortify)
+library(rgl)
+
 term_freq <- read.csv("term_freq.csv",header = TRUE)
 words <- data.frame(fread("words.csv"))
 X <- sparseMatrix(i = term_freq$i, j = term_freq$j, x = term_freq$x)
 dimnames(X)[[2]] <- as.vector(words$word)
-ggplot2::autoplot(
-  princomp(X, scale = TRUE), 
+z <- princomp(X, scale = TRUE, scores = TRUE)
+
+## Gráfica 2d
+ggplot2::autoplot(z, 
   label = FALSE,
   colour = "yellow",
   loadings.label = TRUE,
   loadings.colour = "blue",
   xlim = c(-0.5,1.0),
   ylim = c(-1,1))
+
+## Gráfica 3d
+text3d(z$loadings[,1:3], texts=rownames(z$loadings), col="red")
+coords <- NULL
+for (i in 1:nrow(z$loadings)) {
+  coords <- rbind(coords, rbind(c(0,0,0),z$loadings[i,1:3]))
+}
+lines3d(coords, col="blue", lwd=4)
--- processTweets.py
+++ processTweets.py
@@ -1,14 +1,12 @@
 from DbUtils import DbUtils
-import numpy as np
 import re
 db = DbUtils()
 
 tweets = db.fetchall("tweets", ("text",), sort="id DESC")
 
-print(len(tweets))
 tweets = [re.sub(r'http(s?).* ?', '', x[0]).strip().lower() for x in tweets]
 tweets = list(set(tweets))
-print(len(tweets))
+print("Tweets: " + str(len(tweets)))
 
 words = [  # "star", "wars", "awakens", "force",
          "starwars", "r2", "k2", "jedi", "VII", "episode", "win",  "tickets", "december",
@@ -20,18 +18,14 @@ words = [  # "star", "wars", "awakens", "force",
          "cool", "fuck", "exciting", "honest", "like", "cry", "epic", "need", "really",
          "chance", "only", "official", "pumped", "sucks"
          ]
-i_vector = []
-j_vector = []
-x_vector = []
-for i in range(len(tweets)):
-    for j in range(len(words)):
-        cont = tweets[i].count(words[j])
-        if cont > 0:
-            i_vector.append(i+1)
-            j_vector.append(j+1)
-            x_vector.append(cont)
-g = np.transpose(np.array([i_vector, j_vector, x_vector]))
-np.savetxt("term_freq.csv", g, delimiter=",", header="i,j,x")
+
+with open("term_freq.csv","w") as f:
+    f.write("i,j,x\r\n")
+    for i in range(len(tweets)):
+        for j in range(len(words)):
+            cont = tweets[i].count(words[j])
+            if cont > 0:
+                f.write(str(i+1) + "," + str(j+1) + "," + str(cont) + "\r\n")
 
 with open("words.csv","w") as f:
     f.write("id,word\r\n")
