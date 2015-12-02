32b0ff79160f3188de2bafcaf2e4bdf70329d5ec
Jose Manuel Marron <manemarron@gmail.com>
2015-12-02 17:37:36 -0600

    cambios

--- processTweets.py
+++ processTweets.py
@@ -7,12 +7,13 @@ tweets = db.fetchall(sys.argv[3], ("text",), sort="id DESC")
 
 tweets = [re.sub(r'http(s?).* ?', '', x[0]).strip().lower() for x in tweets]
 tweets = list(set(tweets))
-print("Tweets: " + str(len(tweets)))
 
-with open(sys.argv[1], "rb") as f:
-    words = [x.decode("utf8") for x in f.read().splitlines()]
 
-with open(sys.argv[2], "w") as f:
+
+with open("resources/stopwords.txt","rb") as f:
+    stopwords = f.read().splitlines()
+
+with open("resources/" + sys.argv[2], "w") as f:
     f.write("i,j,x\r\n")
     for i in range(len(tweets)):
         for j in range(len(words)):
